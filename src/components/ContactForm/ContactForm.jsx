a24cae5826f596514107b6ba40d98056ba18754b
natalilevitska <nataliyalevi579@gmail.com>
2022-07-27 10:28:42 +0300

    commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState } from "react";
+import { toast } from "react-toastify";
 import { nanoid } from 'nanoid';
 import {
   PhoneLabel,
@@ -7,39 +8,27 @@ import {
   PhoneForm,
 } from './ContactForm.styled';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
+const ContactForm = ({ allContacts, onSubmit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const isDuplicate = this.checkIfDuplicate();
-    if (isDuplicate) {
-      return alert(`${this.state.name} already exist in your contacts!`);
-    }
-    this.props.onSubmit({ ...this.state, id: nanoid(3) });
-    this.reset();
+    const isDuplicate = (allContacts) => allContacts.name === name;
+    allContacts.some(isDuplicate) ? toast.error(`${name} already exist in your contacts!`)
+      : onSubmit({ name, number, id: nanoid(3) });
+    reset();
   };
-  checkIfDuplicate = () =>
-    this.props.allContacts.some(({ name }) => name === this.state.name);
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName("");
+    setNumber("");
   };
-  render() {
-    const { handleChange, state, handleSubmit } = this;
-    const { name, number } = state;
-    const isBtnDis = Object.values(this.state).some(value => {
-      return !value;
-    });
+ 
+  const isBtnDis = Object.values({ name, number }).some((value) => {
+    return !value;
+  });
+
     return (
       <PhoneForm onSubmit={handleSubmit}>
         <PhoneLabel>
@@ -49,7 +38,7 @@ class ContactForm extends Component {
             name="name"
             value={name}
             placeholder="*Enter name"
-            onChange={handleChange}
+            onChange={(e) => setName(e.target.value)}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title=" Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan "
             required
@@ -62,7 +51,7 @@ class ContactForm extends Component {
             name="number"
             value={number}
             placeholder="*Enter phone"
-            onChange={handleChange}
+            onChange={(e) => setNumber(e.target.value)}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title=" Phone number must be digits and can contain spaces, dashes, parentheses and can start with + "
             required
@@ -74,6 +63,5 @@ class ContactForm extends Component {
       </PhoneForm>
     );
   }
-}
 
 export default ContactForm;
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,61 +1,32 @@
-import React from 'react';
-
+import { useState } from "react";
+import { useLocalStorage } from "react-use";
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import '../../styles/index.css';
 import { Title } from '../Phonebook/Phonebook.styled';
-import * as storage from '../../services/localStorage';
 
-const STORAGE_KET = 'contacts';
-class Phonebook extends React.Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const savedContact = storage.get(STORAGE_KET);
-    if (savedContact) {
-      this.setState({ contacts: savedContact });
-    }
-  }
+const STORAGE_KEY = 'contacts';
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      storage.save(STORAGE_KET, contacts);
-    }
-  }
+const Phonebook = () => {
+  const [contacts, setContacts] = useLocalStorage(STORAGE_KEY, []);
+  const [filter, setFilter] = useState("");
 
-  // створення
-  handleCreate = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-  //видалення
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleCreate = (newContact) => {
+    setContacts([...contacts, newContact]);
   };
 
-  handleFilter = value => this.setState({ filter: value });
-  getFilter = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  const handleDelete = id => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
   };
 
-  render() {
-    const { handleCreate, handleDelete, handleFilter, getFilter } = this;
-    const { filter, contacts } = this.state;
+  const handleFilter = (value) => setFilter(value);
+  const getFilter = () => {
+    return contacts.filter((el) => el.name.toLowerCase().includes(filter.toLowerCase()));
+    
+  };
 
     return (
       <>
@@ -64,27 +35,9 @@ class Phonebook extends React.Component {
         <Title>Contacts</Title>
         <Filter value={filter} onChange={handleFilter} />
         <ContactList lists={getFilter()} onDeleteContact={handleDelete} />
+        <ToastContainer position="top-center" autoClose={3000}  theme="colored"/>
       </>
     );
-  }
-}
+   }
 
 export default Phonebook;
-
-// componentDidMount() {
-//   const contacts = localStorage.getItem('contacts')
-//   const parsedContacts = JSON.parse(contacts)
-
-//   if (parsedContacts) {
-//     this.setState({ contacts: parsedContacts})
-//   }
-// }
-
-// componentDidUpdate(prevState) {
-//   const {contacts} = this.state
-//   const stringifyContacts = JSON.stringify(contacts)
-
-//   if (contacts !== prevState.contacts) {
-//     localStorage.setItem('contacts', stringifyContacts)
-//   }
-// }
